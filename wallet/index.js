23c1cc812e3aa3aac5f01ed762dfbf6fb726613c
Amin Asadi <adoramdev@gmail.com>
2025-07-07 19:07:21 +0330

    make public key for wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,12 @@
 const { STATRING_BALANCE } = require("../config");
+const { ec } = require("../utils");
+
+const keyPair = ec.genKeyPair();
 
 class Wallet {
     constructor() {
         this.balance = STATRING_BALANCE;
+        this.publicKey = keyPair.getPublic("hex");
     }
 }
 
